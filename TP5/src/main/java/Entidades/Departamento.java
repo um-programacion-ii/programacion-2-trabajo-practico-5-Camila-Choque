851f47ddb8a8aee900b24c66b3c09ed06511a70d
Camila-Choque <m.choque@alumno.um.edu.ar>
2025-07-14 19:37:04 -0300

    Relaciones

--- TP5/src/main/java/Entidades/Departamento.java
+++ TP5/src/main/java/Entidades/Departamento.java
@@ -1,10 +1,9 @@
 package Entidades;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -12,12 +11,18 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class Departamento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100, unique = true)
     private String nombre;
+
+    @Column(length = 500)
     private String descripcion;
-    @OneToMany(mappedBy = "departamento", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Empleado> empleados;
+
+    @OneToMany(mappedBy = "departamento", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Empleado> empleados = new ArrayList<>();
 }
